b804315c00b3bcb5872461b63ce99fb4ea5ff7fa
200017414 <200017414@PTL119.ucsal.br>
2022-04-06 09:30:10 -0300

    Code edited

---
+++ simulado-20221-web/src/main/java/br/com/mariojp/condominio/dao/UsuarioDAO.java
@@ -0,0 +1,64 @@
+package br.com.mariojp.condominio.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.com.mariojp.condominio.controllers.LoginController;
+import br.com.mariojp.condominio.model.Usuario;
+
+public class UsuarioDAO {
+
+	
+	
+	private static Map<String, Usuario>  usuarios = new HashMap<String, Usuario>();
+	
+	static {
+		Usuario usuario = new Usuario();
+		usuario.setLogin("root");
+		usuario.setSenha("1234");
+		usuarios.put(usuario.getLogin(), usuario);
+	}
+	
+	
+	public Usuario findByLogin(String login) {
+		if (!login.equals(usuarios)) {
+			Logi
+		}
+		
+		/* for (int i = 0; i < usuarios.size(); i++) {
+			if (!login.equals(usuarios) {
+				
+			} else {
+				
+			}
+		} */
+		return usuarios.get(login);
+		
+	}
+	
+	
+	public void save(Usuario usuario) {
+		usuarios.put(usuario.getLogin(), usuario);
+	}
+	
+	
+	public void delete(Usuario usuario) {
+		delete(usuario.getLogin());
+	}
+	
+	public void delete(String login) {
+		usuarios.remove(login);
+	}
+	
+	public List<Usuario> findAll() {
+		return new ArrayList<Usuario>(usuarios.values());
+	}
+	
+	
+	
+	
+	
+	
+}
